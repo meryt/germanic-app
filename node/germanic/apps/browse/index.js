c31b91acff705f6f69dd78f3ce0605b95506abbd
meryt <meryt@meryt.com>
2014-05-09 14:46:16 -0700

    moved pages functionality to /pages endpoint

--- node/germanic/apps/browse/index.js
+++ node/germanic/apps/browse/index.js
@@ -13,3 +13,4 @@ var app = module.exports = express();
 app.set('views', __dirname + '/templates');
 app.set('view engine', 'jade');
 app.get('/', routes.index);
+app.get('/pages', routes.pages);
--- node/germanic/apps/browse/routes.js
+++ node/germanic/apps/browse/routes.js
@@ -11,8 +11,24 @@ exports.index = function(req, res, next) {
 
     pages.fetch({
         success: function() {
+            res.render('index');
+        },
+        error: function(m, err) {
+            next(err.text);
+        }
+    });
+};
+
+
+exports.pages = function(req, res, next) {
+    var pages = new Pages(null, {
+
+    });
+
+    pages.fetch({
+        success: function() {
             res.locals.sd.PAGES = pages.toJSON();
-            res.render('index', { pages: pages.models });
+            res.render('pages', { pages: pages.models });
         },
         error: function(m, err) {
             console.log("Error in fetch");
